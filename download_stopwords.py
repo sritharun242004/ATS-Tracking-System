776add72f5f9874f572901ff70727f986f9353aa
Tharun Kumar L <123816916+sritharun242004@users.noreply.github.com>
2024-08-25 13:46:26 +0530

    updated

--- download_stopwords.py
+++ download_stopwords.py
@@ -1,3 +1,20 @@
+import ssl
+import certifi
 import nltk
+from nltk.data import find
 
-nltk.download('stopwords')
+# Function to set up SSL context
+def configure_ssl():
+    try:
+        context = ssl.create_default_context(cafile=certifi.where())
+        ssl._create_default_https_context = lambda: context
+    except AttributeError:
+        pass
+
+configure_ssl()
+
+# Download stopwords
+try:
+    nltk.download('stopwords')
+except Exception as e:
+    print(f"Error: {e}")
